94ecc47992fed1b59d5826f9670aae74b0029c5e
Yabin <liuyabin@qq.com>
2013-04-20 18:16:14 +0800

    change password for mysql

--- config/config_global.php
+++ config/config_global.php
@@ -6,7 +6,7 @@ $_config = array();
 // ----------------------------  CONFIG DB  ----------------------------- //
 $_config['db']['1']['dbhost'] = 'localhost';
 $_config['db']['1']['dbuser'] = 'root';
-$_config['db']['1']['dbpw'] = 'songeast123!';
+$_config['db']['1']['dbpw'] = 'tian@walk2';
 $_config['db']['1']['dbcharset'] = 'utf8';
 $_config['db']['1']['pconnect'] = '0';
 $_config['db']['1']['dbname'] = 'ultrax';
