acbf30db551b6094ece558fcde64ed352bfcbe69
Yabin <liuyabin@qq.com>
2013-05-03 20:05:15 +0800

    addfriend

---
+++ source/module/mobile/mobile_addfriend.php
@@ -0,0 +1,24 @@
+<?php
+
+$uid=$_GET['uid'];
+
+$friendid=$_GET['friendid'];
+
+ $_G['uid']=$uid;
+ $profile=c::t('common_member')->fetch_uid_by_user_id($_G['uid']);
+ $_G['username']=$profile[0]['username'];
+ $request=friend_add($friendid);
+ 
+ $_G['uid']=$friendid;
+ $profile=c::t('common_member')->fetch_uid_by_user_id($_G['uid']);
+ $_G['username']=$profile[0]['username'];
+ $reply=friend_add($uid);
+if($request&&$reply)
+{
+	echo json_encode(array('result'=>'success'));
+}
+else
+{
+	echo json_encode(array('result'=>'error'));
+}
+?>
