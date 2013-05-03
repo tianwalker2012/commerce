ecd34ba983c6af841b86257660a49fe550bfecd0
Yabin <liuyabin@qq.com>
2013-05-03 19:44:20 +0800

    remove

--- source/module/mobile/mobile_groupremove.php
+++ source/module/mobile/mobile_groupremove.php
@@ -1,7 +1,7 @@
 <?php
 $plid=$_GET['plid'];
-$toid=$_GET['to'];//数组
-$to=implode(',', $to);
+$persons=$_GET['persons'];//数组
+$to=implode(',', $persons);
 $authorid=DB::fetch_all("select authorid from pre_ucenter_pm_lists where plid=".$plid);
 uc_pm_kickchatpm($plid, $authorid[0]['authorid'], $to);
 $list=get_member_list($plid);
