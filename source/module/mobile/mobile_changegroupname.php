e19e304344ebe255328747a1fbfcd55cf9386a12
Yabin <liuyabin@qq.com>
2013-05-03 19:54:19 +0800

    changename

--- source/module/mobile/mobile_changegroupname.php
+++ source/module/mobile/mobile_changegroupname.php
@@ -1,6 +1,6 @@
 <?php
 $plid=$_GET['plid'];
 $subject=$_GET['name'];
-$subject='greatgroup';
+
 $result=DB::fetch_all("update pre_ucenter_pm_lists set subject ='".$subject."' where plid=".$plid);
 echo json_encode(array(200));
