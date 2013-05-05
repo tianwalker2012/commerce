7412c311e278a5accfba8fb42f5e0f86ab6fd51b
Yabin <liuyabin@qq.com>
2013-05-05 20:42:31 +0800

    update

--- source/module/mobile/mobile_userupdate.php
+++ source/module/mobile/mobile_userupdate.php
@@ -21,31 +21,30 @@ $company=$_POST['company'];
 $industry=$_POST['industry'];
 $gender=$_POST['gender'];
  
-$infomation=array($realname=$_POST['realname'],
-$mobile=$_POST['mobilephone'],
-$postion=$_POST['postion'],
-$location=$_POST['location'],
-$latitude=$_POST['latitude'],
-$longitude=$_POST['longitude'],
-$company=$_POST['company'],
-$industry=$_POST['industry'],
-$gender=$_POST['gender']
-);*/
+
  $infomation=array(
 	'realname'=>'nicke',
 	'mobile'=>'15852951529',
-	'postion'=>'国家主席',
+	'position'=>'国家主席',
 	'location'=>'北京',
 	'company'=>'netscout',
 	'industry'=>'IT',
 	'gender'=>1,
 	'uid'=>1
+);*/
+ $infomation=array('realname'=>$_GET['realname'],
+					'mobile'=>$_GET['mobilephone'],
+					'position'=>$_GET['position'],
+					'location'=>$_GET['location'],
+					'company'=>$_GET['company'],
+					'industry'=>$_GET['industry'],
+					'gender'=>$_GET['gender'],
+					'uid'=>$_GET['uid']
 );
- 
 $result=c::t('common_member_profile')->updateinfo($infomation);
 if($result)
 {
-	echo json_encode(array('result'=>'success'));
+	echo json_encode(array('result'=>200));
 }
 
  
