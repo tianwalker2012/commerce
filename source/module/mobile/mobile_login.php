b3ffbec729ece27fbe6b08ea355fd0c566a811a5
Yabin <liuyabin@qq.com>
2013-05-05 20:18:13 +0800

    login

--- source/module/mobile/mobile_login.php
+++ source/module/mobile/mobile_login.php
@@ -10,30 +10,29 @@ if($profile)
 {
 	if($profile['avatarstatus'])
 	{
-		echo $profile['avatarstatus']=discuz_uc_avatar($profile['uid'],'small',true);
+		  $profile['avatarstatus']=discuz_uc_avatar($profile['uid'],'small',true);
 	}
 	$back=array('uid'=>$profile['uid'],
 		        'email'=>$profile['email'],	 
 		        'username'=>$profile['username'],	 
 		        'avatar'=>$profile['avatarstatus'],
-	            'mobilephone'=>$profiledetail['mobile'],
-	            'postion'=>$profiledetail['postion'],
-	            'location'=>$profiledetail['resideprovince'].$profiledetail['residecity'].$profiledetail['residedist'].$profiledetail['residecommunity'].$profiledetail['residesuite'],	 
-                'latitude'=>$profiledetail['latitude'],
-	            'longitude'=>$profiledetail['longitude'],
-	            'bio'=>$profiledetail['bio'],
-	            'company'=>$profiledetail['company'],
-	            'industry'=>$profiledetail['industry'],
-	            'gender'=>$profiledetail['gender'],
+	            'mobilephone'=>$profiledetail[1]['mobile'],
+	            'postion'=>$profiledetail[1]['postion'],
+	            'location'=>$profiledetail[1]['resideprovince'].$profiledetail['residecity'].$profiledetail['residedist'].$profiledetail['residecommunity'].$profiledetail['residesuite'],	 
+                'latitude'=>$profiledetail[1]['latitude'],
+	            'longitude'=>$profiledetail[1]['longitude'],
+	            'bio'=>$profiledetail[1]['bio'],
+	            'company'=>$profiledetail[1]['company'],
+	            'industry'=>$profiledetail[1]['industry'],
+	            'gender'=>$profiledetail[1]['gender'],
 	            'friendnum'=>$num[0]['num'],
 	            'postnum'=>$postnum[0]['num'],
 	);
-	echo json_encode($back=array('result'=>'success',
-			                      'data'=>$back));
+	echo json_encode($back);
 }
 else
  { 
- 	echo  json_encode($back=array('result'=>'error'));
+ 	header('HTTP/1.1 200 OK');  
  }
  
 ?>
