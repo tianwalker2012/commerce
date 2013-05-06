ec1cc927cc47ffde4835bc0f32270b00d4396d27
Yabin <liuyabin@qq.com>
2013-05-06 10:29:12 +0800

    20130506login

--- source/module/mobile/mobile_login.php
+++ source/module/mobile/mobile_login.php
@@ -1,9 +1,10 @@
 <?php
-  
+   
 $username=$_GET['username'];
 $pwd=$_GET['password'];
 $profile=c::t('common_member')->check_login($username,$pwd);
 $profiledetail=c::t('common_member_profile')->fetch_all($profile['uid']);
+ 
 $num=c::t('home_friend')->friendnum($profile['uid']);
 $postnum=c::t('forum_post')->postnum($profile['uid']);
 if($profile)
@@ -13,15 +14,15 @@ if($profile)
 		        'email'=>$profile['email'],	 
 		        'username'=>$profile['username'],	 
 		        'avatar'=>$profile['avatarstatus'],
-	            'mobilephone'=>$profiledetail[1]['mobile'],
-	            'postion'=>$profiledetail[1]['postion'],
-	            'location'=>$profiledetail[1]['resideprovince'].$profiledetail['residecity'].$profiledetail['residedist'].$profiledetail['residecommunity'].$profiledetail['residesuite'],	 
-                'latitude'=>$profiledetail[1]['latitude'],
-	            'longitude'=>$profiledetail[1]['longitude'],
-	            'bio'=>$profiledetail[1]['bio'],
-	            'company'=>$profiledetail[1]['company'],
-	            'industry'=>$profiledetail[1]['industry'],
-	            'gender'=>$profiledetail[1]['gender'],
+	            'mobilephone'=>$profiledetail[$profile['uid']]['mobile'],
+	            'postion'=>$profiledetail[$profile['uid']]['postion'],
+	            'location'=>$profiledetail[$profile['uid']]['resideprovince'].$profiledetail['residecity'].$profiledetail['residedist'].$profiledetail['residecommunity'].$profiledetail['residesuite'],	 
+                'latitude'=>$profiledetail[$profile['uid']]['latitude'],
+	            'longitude'=>$profiledetail[$profile['uid']]['longitude'],
+	            'bio'=>$profiledetail[$profile['uid']]['bio'],
+	            'company'=>$profiledetail[$profile['uid']]['company'],
+	            'industry'=>$profiledetail[$profile['uid']]['industry'],
+	            'gender'=>$profiledetail[$profile['uid']]['gender'],
 	            'friendnum'=>$num[0]['num'],
 	            'postnum'=>$postnum[0]['num'],
 	);
