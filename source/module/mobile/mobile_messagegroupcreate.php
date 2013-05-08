8871e3556cb2871981653e7af41bdbe692cfa1ce
Yabin <liuyabin@qq.com>
2013-05-08 16:47:21 +0800

    querychat

--- source/module/mobile/mobile_messagegroupcreate.php
+++ source/module/mobile/mobile_messagegroupcreate.php
@@ -1,21 +1,20 @@
 <?php
  
-$from=$_GET['authorid '];
+$from=$_GET['authorid'];
 $persons=$_GET['persons'];//数组
 $to=implode(',', $persons);
 $subject=$_GET['name'];
 $message=$_GET['message'];
  
-
  /*
 $from=1;
 $toid=array(2,3,4,5);//数组
-$to=implode(',', $toid);
-$subject="this is a test";
- $message="this is a testthis is a testthis is a testthis is a testthis is a test"; 
- */
+$to=implode(',', $toid);*/
+$subject="New Group";
+$message="A New Group"; 
+  
  
-   $result=uc_pm_send($from, $to, $subject, $message,1,0,0,1);
- $sql="select plid from pre_ucenter_pm_indexes where pmid=$result limit 1";
+$result=uc_pm_send($from, $to, $subject, $message,1,0,0,1);
+$sql="select plid from pre_ucenter_pm_indexes where pmid=$result limit 1";
 $data=DB::fetch_all($sql);
 echo json_encode(array('plid'=>$data[0]['plid']));
