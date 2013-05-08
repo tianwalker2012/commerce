f1aed1ee749f166cd228da626bc5deb67d9bd337
Yabin <liuyabin@qq.com>
2013-05-08 12:14:17 +0800

    querychat

--- source/module/mobile/mobile_messagesession.php
+++ source/module/mobile/mobile_messagesession.php
@@ -11,45 +11,35 @@ foreach ($result as $k=>$v) {
      array_push($plid, $v['plid']);
 } 
   
-$num=count($plid);
 $lastpm=array();
  foreach ($plid as $k=>$v){
     $v1=$v%10;
- 	 $sql ="select m.pmid,l.subject,m.authorid ,l.lastmessage,l.min_max,l.pmtype,m.plid, m.message content, m.dateline from pre_ucenter_pm_messages_"."$v1"." m ";
+    $sql ="select m.pmid,l.subject,m.authorid ,l.lastmessage,l.min_max,l.pmtype,m.plid, m.message content, m.dateline,m.pmtype,m.resource_url from pre_ucenter_pm_messages_"."$v1"." m ";
  	$sql.=" inner join pre_ucenter_pm_lists l on l.plid=m.plid";
-    $sql.=" where m.plid=".$v."  order by m.dateline limit 1";
+    $sql.=" where m.plid=".$v."  order by m.dateline desc limit 1";
     $last_pm=DB::fetch_all($sql);
     $lastpm[]=$last_pm;
  }
  $lastpm=get_array_inner($lastpm);
  foreach ($lastpm as $k=>$v){
- 		$lastmessage = unserialize($v['lastmessage']);
-		if($lastmessage['lastauthorid']) {
-			$lastpms['lastauthorid'] = $lastmessage['lastauthorid'];
-			$lastpms['lastauthor'] = $lastmessage['lastauthor'];
-			$lastpms['lastsummary'] = $lastmessage['lastsummary'];
-		} else {
-			$lastpms['lastauthorid'] = $lastmessage['firstauthorid'];
-			$lastpms['lastauthor'] = $lastmessage['firstauthor'];
-			$lastpms['lastsummary'] = $lastmessage['firstsummary'];
-		}
-	   $author=$lastpms['lastauthorid'];
+ 	 
+	    $author=$v['authorid'];
 		$to="";
-	    $v['min_max']."<br>";
 		if($v['min_max']){
 	    $minmax=explode('_', $v['min_max']);
 		$to=array_diff($minmax,array($author));
 		$to=array_values($to);
-		  $to=$to[0];
+	    $to=$to[0];
 		}
 		$lastpm[$k]['author']=$author;
 	    $lastpm[$k]['to']=$to;
-	 	 
+		unset($lastpm[$k]['min_max']);
 	 	unset($lastpm[$k]['lastmessage']);
 		 
  }
+ 
  if(!empty($lastpm)){
-    echo json_encode(array('total_count'=>$num,'data'=>$lastpm));
+    echo json_encode(array('total_count'=>count($plid),'data'=>$lastpm));
  }else {
  	echo json_encode(array('total_count'=>0));
  } 
