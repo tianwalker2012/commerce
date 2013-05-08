43173913bd96635e034f65e92bd651a73228e307
Yabin <liuyabin@qq.com>
2013-05-08 11:17:30 +0800

    querychat

--- source/module/mobile/mobile_querychat.php
+++ source/module/mobile/mobile_querychat.php
@@ -1,7 +1,6 @@
 <?php
 $p1=$_GET['p1'];
 $p2=$_GET['p2'];
-$plid=$_GET['plid'];
 $start=$_GET['start'];
 $limit=$_GET['limit'];
 if($p1>$p2)
@@ -19,14 +18,15 @@ if($result)
 {
 $plid=$result[0]['plid'];
 $lastint=$plid % 10;
-$sql="select pmid,plid,authorid author ,message,dateline from pre_ucenter_pm_messages_$lastint order by dateline desc limit $start,$limit ";
+$sql="select pmid,plid,authorid author ,message,dateline,pmtype  from pre_ucenter_pm_messages_$lastint where plid=$plid  order by dateline desc limit $start,$limit ";
 $result=DB::fetch_all($sql);
 foreach ($result as $k=>$v){
-	$result[$k]['to']==$v['authorid']==$p1?$p2:$p1;
+	//echo $v['author'].'................'.$v['message']."<br>";
+	$result[$k]['to']=($v['author']==$p1)?$p2:$p1;
 }
-echo json_encode(array('total_count'=>count($result),'data'=>$result));
+ echo json_encode(array('total_count'=>count($result),'data'=>$result));
 }
 else 
 {
-echo   json_encode(array('total_count'=>0));
+ echo   json_encode(array('total_count'=>0));
 }
