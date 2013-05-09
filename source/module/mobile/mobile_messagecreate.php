5736eeba38538cf8020887f855a6e9eb9961afb0
Yabin <liuyabin@qq.com>
2013-05-09 19:08:06 +0800

    query cycle

--- source/module/mobile/mobile_messagecreate.php
+++ source/module/mobile/mobile_messagecreate.php
@@ -1,16 +1,32 @@
 <?php
-$toid=$_GET['to'];
-$subject=$_GET['subject'];
-$message=$_GET['message'];
+ 
 $from=$_GET['from'];
-$plid=$_GET['plid']; 
+$to=$_GET['to'];
+$message=$_GET['message'];
+$pmtype=$_GET['pmtype'];
 $type=$_GET['type'];
-   if(isset($plid)&&!empty($plid))
-   {
-   	$msg_id=sendpm($toid, $subject, $message,$from,$plid);
-   }
-   else {
-   $msg_id=sendpm($toid, $subject, $message,$from);
-   }
- echo json_encode(array('pmid'=>$msg_id));
- ?>
+$plid= $_GET['plid'];
+
+ /*$from=1;
+$toid=array(2,3,4,5);//数组
+$to=implode(',', $toid);
+$subject="New Group";
+$message="A New Group"; 
+   
+
+echo "<pre>";
+$from=1;
+$message="what is  new group";
+$subject="a_message";
+$pmtype=1;
+$plid=44;*/
+$pmid=0;
+if($pmtype==1){
+$pm=DB::fetch_all("select max(pmid) pmid from pre_ucenter_pm_indexes where plid=".$plid);
+$pmid=$pm[0]['pmid'];
+} 
+
+$result=uc_pm_send($from, $to, $subject, $message,1,$pmid,0,$pmtype);
+$sql="select max(pmid) pmid from pre_ucenter_pm_indexes ";
+$data=DB::fetch_all($sql);
+echo json_encode(array('plid'=>$data[0]['plid']));
